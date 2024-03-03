20c5344009f54ef3c841ebee361df7bd52a38ff4
pskink <pskink@gmail.com>
2024-03-03 15:53:55 +0100

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,47 @@
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:flow_stack/flow_stack.dart';
+import 'package:flow_stack/flow_painting_context_extension.dart';
+import 'package:flutter/material.dart';
+
+part 'overview.dart';
+part 'delegates.dart';
+part 'page_turn.dart';
+
+final demos = [
+  ('overview', '_FlowStackOverview()', _FlowStackOverview()),
+  ('delegates', '_FlowStackDelegates()', _FlowStackDelegates()),
+  ('page turn', '_FlowStackPageTurn()', _FlowStackPageTurn()),
+];
+
+main() {
+  runApp(MaterialApp(
+    scrollBehavior: const MaterialScrollBehavior().copyWith(
+      dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch},
+    ),
+    theme: ThemeData.light(useMaterial3: false),
+    home: Scaffold(
+      body: Builder(
+        builder: (context) {
+          return ListView(
+            children: [
+              for (final (title, subtitle, body) in demos)
+                ListTile(
+                  title: Text('[$title]'),
+                  subtitle: Text(subtitle),
+                  onTap: () {
+                    final route = MaterialPageRoute(builder: (ctx) => Scaffold(
+                      appBar: AppBar(),
+                      body: body,
+                    ));
+                    Navigator.of(context).push(route);
+                  },
+                ),
+            ],
+          );
+        },
+      ),
+    ),
+  ));
+}
